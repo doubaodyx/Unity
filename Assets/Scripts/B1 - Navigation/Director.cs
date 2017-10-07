a158f3a226a7b724df3254991bdc97bdf041a132
liuchengtian <liuchengtian@gmail.com>
2017-10-07 16:36:04 -0400

    add movement and director

---
+++ Assets/Scripts/B1 - Navigation/Director.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Director : MonoBehaviour {
+
+    public OurCameraController camControl;
+    public GameObject[] allAgents;
+    public Hashtable stoppedAgents;
+    public bool beginBrakes;
+
+    void Awake()
+    {
+        beginBrakes = false;
+        allAgents = GameObject.FindGameObjectsWithTag("Soldier");
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            float horizontalArrow = Input.GetAxis("Horizontal");
+            float verticalArrow = Input.GetAxis("Vertical");
+            float horizontalMouse = Input.GetAxis("Mouse X");
+            float verticalMouse = Input.GetAxis("Mouse Y");
+            camControl.MoveCamera(horizontalArrow, verticalArrow, horizontalMouse, verticalMouse);
+        }
+        if (beginBrakes)
+        {
+            foreach (GameObject agent in allAgents)
+            {
+                if (!stoppedAgents.ContainsKey(agent.name))
+                {
+                    foreach (string agentName in stoppedAgents.Keys)
+                    {
+                        GameObject stoppedAgent = (GameObject)stoppedAgents[agentName];
+                        if (Vector3.Distance(agent.transform.position, stoppedAgent.transform.position) <= 5.0f)
+                        {
+                            stoppedAgents.Add(agent.name, agent);
+                            agent.GetComponent<NavMeshAgent>().Stop();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
