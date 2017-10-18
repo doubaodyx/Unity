13269a60e26f60fe2ab73117b7ded80539552cd5
Taner <tjdoganay@gmail.com>
2017-10-18 16:18:12 -0400

    Added CrowdRoom
    NavMeshAgents inside this room attempt to evacuate at the start of the simulation. The same braking mechanism is used as in B1.

---
+++ Assets/Scripts/B2 - Navigation and Animation/CrowdMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CrowdMovement : MonoBehaviour {
+
+    private Animator anim;
+    private NavMeshAgent navMeshAgent;
+    private Renderer matRender;
+    private bool isWalking;
+    private bool isRunning;
+    private bool isJumping;
+    private GameObject target;
+    public Director director;
+	public Camera camera;
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        matRender = GetComponent<Renderer>();
+        target = GameObject.Find("car_3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (director.beginBrakes == true)
+        {
+            director.beginBrakes = false;
+            director.stoppedAgents.Clear();
+        }
+        navMeshAgent.Resume();
+        navMeshAgent.destination = target.transform.position;
+        isWalking = true;
+        navMeshAgent.updatePosition = true;
+        navMeshAgent.updateRotation = true;
+        navMeshAgent.nextPosition = transform.position;
+        if (Vector3.Distance(transform.position, navMeshAgent.destination) < 1.0f)
+        {
+            if (director.beginBrakes != true)
+            {
+                navMeshAgent.Stop();
+                director.beginBrakes = true;
+                director.stoppedAgents = new Hashtable();
+                director.stoppedAgents.Add(gameObject.name, gameObject);
+            }
+        }
+    }
+}
